c2958fceabac35b8976b3a7bbff84a33aa3f6c05
Hanna Nabet <hanna.nabet@yahoo.fr>
2019-12-12 21:39:07 +0100

    to finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+    Article.create(
+      title: Faker::Quotes::Shakespeare.as_you_like_it_quote,
+      content:  Faker::Quotes::Shakespeare.hamlet_quote,
+    )
+end
